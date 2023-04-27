e75ac6d4cee7903d141cbe89d832b29a0091105d
MatteoCoffman <matteo.coffman@gmail.com>
2023-04-26 19:44:18 -0500

    Included NBA Odds

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 BASE_URL = "https://api.sportsdata.io"
+BAKER_URL = "https://baker-api.sportsdata.io"
 
 
 @app.route("/api/scores", methods=["POST"])
@@ -18,6 +19,27 @@ def get_scores():
     )
     scores = response.json()
     return jsonify(scores)
+
+
+@app.route("/api/in-game-odds", methods=["POST"])
+def get_in_game_odds():
+    date = request.json["date"]
+    response = requests.get(
+        f"{BASE_URL}/v3/nba/odds/json/LiveGameOddsByDate/{date}?key={API_KEY}"
+    )
+    in_game_odds = response.json()
+    return jsonify(in_game_odds)
+
+
+@app.route("/api/best-bets", methods=["POST"])
+def best_bets():
+    if request.method == "POST":
+        date = request.json["date"]
+        response = requests.get(
+            f"{BAKER_URL}/baker/v2/nba/best-bets/{date}?key={API_KEY}"
+        )
+        best_bets = response.json()
+        return jsonify(best_bets)
 
 
 if __name__ == "__main__":
--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -3,9 +3,39 @@ import React, { useState } from 'react';
 const Homepage = () => {
     const [date, setDate] = useState('');
     const [scores, setScores] = useState([]);
+    const [bestBets, setBestBets] = useState([]);
+    const [inGameOdds, setInGameOdds] = useState([]);
+
+    const fetchInGameOdds = async () => {
+        const response = await fetch('/api/in-game-odds', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ date }),
+        });
+        const data = await response.json();
+        console.log('In-Game Odds:', data);
+        setInGameOdds(data);
+    };
+
+    const fetchBestBets = async () => {
+        const response = await fetch('/api/best-bets', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ date }),
+        });
+        const data = await response.json();
+        console.log('Best Bets:', data);
+        setBestBets(data);
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        fetchInGameOdds();
+        fetchBestBets();
         try {
             const response = await fetch('/api/scores', {
                 method: 'POST',
@@ -23,7 +53,6 @@ const Homepage = () => {
 
     return (
         <div>
-            <h1>NBA Scores</h1>
             <form onSubmit={handleSubmit}>
                 <input
                     type="date"
@@ -38,6 +67,44 @@ const Homepage = () => {
                         {score.HomeTeam} {score.HomeTeamScore} vs {score.AwayTeam} {score.AwayTeamScore}
                     </div>
                 ))}
+            </div>
+            <div>
+                <h2>In-Game Odds</h2>
+                {inGameOdds.map((gameOdds, index) => {
+                    const odds = gameOdds.LiveOdds[0];
+                    if (!odds) return null;
+                    const { HomeMoneyLine, AwayMoneyLine, HomePointSpread, AwayPointSpread, OverUnder, OverPayout, UnderPayout } = odds;
+                    return (
+                        <div key={index}>
+                            <p>
+                                {gameOdds.HomeTeamName} Money Line: {HomeMoneyLine} vs {gameOdds.AwayTeamName} Money Line: {AwayMoneyLine}
+                            </p>
+                            <p>
+                                {gameOdds.HomeTeamName} Point Spread: {HomePointSpread} vs {gameOdds.AwayTeamName} Point Spread: {AwayPointSpread}
+                            </p>
+                            <p>Over/Under: {OverUnder}</p>
+                            <p>
+                                Over Payout: {OverPayout} / Under Payout: {UnderPayout}
+                            </p>
+
+                        </div>
+                    );
+                })}
+            </div>
+            <div>
+                <h3>Best Bets</h3>
+                {Array.isArray(bestBets) ? (
+                    bestBets.map((bestBet, index) => (
+                        <div key={index}>
+                            <p>
+                                {bestBet.HomeTeam} vs {bestBet.AwayTeam}
+                            </p>
+                            <p>Best Bet: {bestBet.BestBet}</p>
+                        </div>
+                    ))
+                ) : (
+                    <p>No best bets available</p>
+                )}
             </div>
         </div>
     );
