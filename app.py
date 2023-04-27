1cc467213f45c444575b2814e9b865300ec20965
Samir <scortezz0624@gmail.com>
2023-04-27 04:42:43 -0500

    created static folder with login and signup css files with images

--- app.py
+++ app.py
@@ -72,35 +72,36 @@ def signup_user():
         return flask.render_template("signup.html")
 
 
-@app.route("/api/scores", methods=["POST", "GET"])
+@app.route("/api/scores", methods=["GET", "POST"])
 def get_scores():
-    date = flask.request.json["date"]
-    response = requests.get(
-        f"{BASE_URL}/v3/nba/scores/json/GamesByDate/{date}?key={API_KEY}"
-    )
+    if flask.request.method == "POST":
+        date = flask.request.json["date"]
+        response = requests.get(
+            f"{BASE_URL}/v3/nba/scores/json/GamesByDate/{date}?key={API_KEY}"
+        )
     scores = response.json()
     return flask.jsonify(scores)
 
 
 @app.route("/api/in-game-odds", methods=["POST"])
 def get_in_game_odds():
-    date = request.json["date"]
+    date = flask.request.json["date"]
     response = requests.get(
         f"{BASE_URL}/v3/nba/odds/json/LiveGameOddsByDate/{date}?key={API_KEY}"
     )
     in_game_odds = response.json()
-    return jsonify(in_game_odds)
+    return flask.jsonify(in_game_odds)
 
 
 @app.route("/api/best-bets", methods=["POST"])
 def best_bets():
-    if request.method == "POST":
-        date = request.json["date"]
+    if flask.request.method == "POST":
+        date = flask.request.json["date"]
         response = requests.get(
             f"{BAKER_URL}/baker/v2/nba/best-bets/{date}?key={API_KEY}"
         )
         best_bets = response.json()
-        return jsonify(best_bets)
+        return flask.jsonify(best_bets)
 
 
 if __name__ == "__main__":
